d30ba51e1a8f5e81cc1bf17462c558ceff85aa84
Soham <sohamnandwalsn@gmail.com>
2026-04-25 18:16:05 +0530

    feat: User system with comment, edit and delete feature

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,6 @@ mongoose.connect('mongodb://localhost:27017/wallofshame')
 const nodesSchema = new mongoose.Schema({
   id: String,
   type: String,
-  owner: String,
   position : {
     x: Number,
     y: Number
@@ -23,16 +22,21 @@ const nodesSchema = new mongoose.Schema({
   data: {
     title: String,
     label: String,
-    color: String
-  },
-  comments: {
-    comment: String,
-    owner: String
+    color: String,
+    owner: String,
+    comments: [{
+      commentid: String,
+      comment: String,
+      commenter: String
+    }]
   }
+  
 })
 
 const idScehma = new mongoose.Schema({
-  id: {type:String, default: "0"}
+  nodeid: {type:String, default: "0"},
+  userid : {type: String, default: "0"},
+  commentid : {type: String, default: "0"}
 })
 
 const Node = mongoose.model('Node', nodesSchema)
@@ -60,14 +64,25 @@ app.get('/ids', async (req, res) => {
   }
 });
 
+app.get('/newuser', async (req, res) => {
+  try{
+    const id = await IDs.findOne() || new IDs()
+    id.userid = (parseInt(id.userid) + 1).toString()
+    await id.save()
+    res.status(200).json({userid: id.userid})
+  }catch(err){
+    res.status(500).json({ message: err.message })
+  }
+})
+
 app.post('/nodes', async (req, res) => {
   try {
     const newNode = new Node(req.body)
     
     const id = await IDs.findOne() || new IDs()
-    id.id = (parseInt(id.id) + 1).toString()
+    id.nodeid = (parseInt(id.nodeid) + 1).toString()
     
-    newNode.id = id.id
+    newNode.id = id.nodeid
     const data = await newNode.save()
     
     console.log(data, id)
@@ -87,6 +102,73 @@ app.post('/UpdatePosition', async (req, res) => {
     console.log(data)
     res.status(200).json(data)
   } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
+
+app.delete('/delnode', async (req, res) => {
+  try {
+    const node = await Node.findOne({id: req.body.id});
+    console.log("delteing node: ", node)
+    await Node.deleteOne(node)
+    res.status(204).json({ message: "Node deleted" })
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
+
+app.post('/editNode', async (req, res) => {
+  try {
+    const node = await Node.findOne({id: req.body.id});
+    node.data.title = req.body.data.title
+    node.data.label = req.body.data.label
+    node.data.color = req.body.data.color
+    const data = await node.save()
+    console.log(data)
+    res.status(200).json(data)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
+
+app.post('/commentNode', async (req, res) => {
+  try {
+    const node = await Node.findOne({id: req.body.nodeid});
+    const commentId = await IDs.findOne() || new IDs()
+    commentId.commentid = (parseInt(commentId.commentid) + 1).toString()
+    commentId.save()
+
+    node.data.comments.push({
+      commentid: commentId.commentid,
+      comment: req.body.commentBody.comment,
+      commenter: req.body.commentBody.commenter
+    })
+    const data = await node.save()
+    console.log("Commented:", data)
+    res.status(200).json(data)
+
+  }catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
+
+app.post('/deleteComment', async (req, res) => {
+  try {
+    const node = await Node.findOne({id: req.body.nodeid});
+    node.data.comments = node.data.comments.filter(c => c.commentid !== req.body.commentid)
+    const data = await node.save()
+    console.log("Deleted Comment:", data)
+    res.status(200).json(data)
+  }catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
+
+app.post('/userNodes', async (req, res) => {
+  try {
+    const nodes = await Node.find({"data.owner": req.body.userid})
+    res.status(200).json(nodes)
+  }catch (err) {
     res.status(400).json({ message: err.message })
   }
 })
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,8 @@ import './App.css'
 import '@xyflow/react/dist/base.css';
 import { Background, ReactFlow, useNodesState, Controls, useReactFlow } from '@xyflow/react'
 import { useState, useEffect } from 'react';
-
+// import pencil from './assets/pencil.svg';
+// import dustbin from './assets/dustbin.svg';
 
 
 const colors = {
@@ -19,14 +20,44 @@ const colIDS = {
   3: 'green',
   4: 'yellow'
 }
+const inverseColIDS = {
+  'red': 1,
+  'blue': 2,
+  'green': 3,
+  'yellow': 4
+}
+
+// const editNode = async (node) => {
+
+// }
+
+
+
 
 function stickyNote(node) {
   // console.log(node.data, style)
+  // console.log(node, localStorage.getItem("userid"))
   return (
-    <div className='p-2 rounded-md border-2 w-48 max-h-[20vh] overflow-y-auto' style={{ backgroundColor: colors[node.data.color].light, borderColor: colors[node.data.color].dark }}>
-      <h1 className='font-mono font-bold'>{node.data.title}</h1>
+    <div className='relative group p-2 rounded-md border-2 min-w-48 max-w-96 max-h-[30vh] overflow-y-auto' style={{ backgroundColor: colors[node.data.color].light, borderColor: colors[node.data.color].dark }}>
+      {node.data.owner === localStorage.getItem("userid") && (
+        <div className='absolute top-2 right-2 flex flex-row gap-1 z-10 opacity-0 group-hover:opacity-100'>
+          <button className='w-4 h-4' onClick={() => node.data.editNode(node)}>
+            <img src="/pencil.svg" alt="Edit" className='w-4 h-4' />
+          </button>
+          <button className='w-4 h-4' onClick={() => node.data.deleteNode(node.id)}>
+            <img src="/trash.svg" alt="Delete" className='w-4 h-4' />
+          </button>
+        </div>
+      )
+      }
+
+      <h1 className='font-mono font-bold  min-h-4'>{node.data.title}</h1>
       <hr></hr>
-      <h1 className='font-mono'>{node.data.label}</h1>
+      <h3 className='font-mono'>{node.data.label}</h3>
+
+      <button className='absolute bottom-2 right-2 w-4 h-4' onClick={() => node.data.commentNode(node.id)}>
+        <img src="/comment.svg" alt="Delete" className='w-4 h-4' />
+      </button>
     </div>)
 }
 
@@ -73,8 +104,8 @@ function stickyNote(node) {
 const nodeTypes = {
   stickyNote: stickyNote
 }
-const MAXHEIGHT = window.innerHeight * 0.2 + 20 //20vh
-const MAXWIDTH = 192 + 20 //w-48
+const MAXHEIGHT = window.innerHeight * 0.3 + 20 //30vh
+const MAXWIDTH = 384 + 20 //w-48
 
 
 function App() {
@@ -83,17 +114,172 @@ function App() {
   const [label, setLabel] = useState("")
   const [colorID, setColorId] = useState(1)
   const { getIntersectingNodes, screenToFlowPosition, setCenter, getZoom } = useReactFlow()
+  const [userid, setUserId] = useState("0")
   // const {addNodes , screenToFlowPosition } =useReactFlow()
-  async function callServer() {
-    console.log("Fetch Data")
-    var res = await fetch("http://localhost:8000/nodes")
-    var body = await res.json()
-    console.log(body)
-    setNodes(body)
-  }
+
   useEffect(() => {
+    async function loginUser() {
+      let uid = localStorage.getItem("userid")
+      if (!uid) {
+        localStorage.setItem('userid', 'pending...');
+        try {
+          const res = await fetch("http://localhost:8000/newuser")
+          const body = await res.json()
+          localStorage.setItem("userid", body.userid)
+          console.log("New User ID: ", body.userid)
+          uid = body.userid
+        } catch (err) {
+          console.log("Error: ", err)
+        }
+      }
+      setUserId(uid)
+    }
+    loginUser()
+  })
+
+
+
+  useEffect(() => {
+    async function callServer() {
+      console.log("Fetch Data")
+      var res = await fetch("http://localhost:8000/nodes")
+      var body = await res.json()
+      console.log(body)
+      setNodes(body)
+    }
     callServer()
   }, [setNodes])
+
+  const deleteNode = async (id) => {
+    try {
+      const res = await fetch(`http://localhost:8000/delnode`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id: id })
+      })
+
+      if (res.ok) {
+        console.log("Node deleted successfully")
+        setNodes((nodes) => nodes.filter((n) => n.id !== id))
+      }
+    } catch (err) {
+      console.log("Error: ", err)
+    }
+  }
+
+  const [editNodeId, setEditNodeId] = useState("")
+
+  const [editTitle, setEditTitle] = useState("");
+  const [editLabel, setEditLabel] = useState("");
+  const [editColorId, setEditColorId] = useState(1);
+
+  const editNode = async (node) => {
+    setEditTitle(node.data.title)
+    setEditLabel(node.data.label)
+    setEditColorId(inverseColIDS[node.data.color])
+
+    setEditNodeId(node.id)
+  }
+
+  const saveEditNode = async () => {
+    if (editTitle.trim() === "" || editLabel.trim() === "") {
+      alert("Invalid or blank note.")
+      return
+    }
+    try {
+      const res = await fetch('http://localhost:8000/editNode', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          id: editNodeId,
+          data: { title: editTitle, label: editLabel, color: colIDS[editColorId] }
+        }),
+      });
+      if (res.ok) {
+        const data = await res.json();
+        console.log("Edited:", data)
+        setNodes((nodes) => nodes.map((n) => data.id === n.id ? { ...n, data: { ...n.data, title: data.data.title, label: data.data.label, color: data.data.color } } : n))
+        setEditNodeId("")
+      }
+    } catch (error) {
+      console.error("Failed to update database:", error);
+    }
+  }
+
+
+
+  const [commentNodeId, setCommentNodeId] = useState("")
+  const [comment, setComment] = useState("")
+
+  const commentNode = async (id) => {
+    setCommentNodeId(id)
+  }
+
+  const saveComment = async () => {
+    if (comment.trim() === "") {
+      alert("Invalid or blank comment.")
+      return
+    }
+    const commentBody = {
+      commentid: "0",
+      comment: comment,
+      commenter: userid
+    }
+    console.log("save comment", commentBody)
+
+    // setNodes((nodes) => nodes.map((n) => {
+    //   if (n.id === commentNodeId) {
+    //     const oldComments = n.data.comments || []
+    //     return { ...n, data: { ...n.data, comments: [...oldComments, commentBody] } }
+    //   }
+    //   return n
+    // }))
+    try {
+      const res = await fetch('http://localhost:8000/commentNode', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          nodeid: commentNodeId,
+          commentBody: commentBody
+        }),
+      });
+      if (res.ok) {
+        const data = await res.json();
+        console.log("Commented:", data)
+        setNodes((nodes) => nodes.map((n) => data.id === n.id ? { ...n, data: { ...n.data, comments: data.data.comments } } : n))
+      }
+    } catch (error) {
+      console.error("Failed to update database:", error);
+    }
+    setComment("")
+  }
+
+  const deleteComment = async (commentid) => {
+    try {
+      const res = await fetch('http://localhost:8000/deleteComment', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          nodeid: commentNodeId,
+          commentid: commentid
+        }),
+      });
+      if (res.ok) {
+        const data = await res.json();
+        console.log("Deleted Comment:", data)
+        setNodes((nodes) => nodes.map((n) => n.id === data.id ? { ...n, data: { ...n.data, comments: data.data.comments } } : n))
+      }
+    } catch (error) {
+      console.error("Failed to update database:", error);
+    }
+  }
+
+
+
+
+
 
   const isOverLapping = (tX, tY) => {
     for (let node of nodes) {
@@ -120,6 +306,13 @@ function App() {
   }
 
   const onAddNode = async () => {
+
+    if (title.trim() === "" || label.trim() === "") {
+      alert("Invalid or blank note.")
+      return
+    }
+
+
     const flow = document.querySelector('.react-flow')
     const pos = screenToFlowPosition({ x: flow.clientWidth / 2, y: flow.clientHeight / 2 })
     const safePos = findPosition(pos.x, pos.y)
@@ -128,10 +321,10 @@ function App() {
     const newNode = {
       id: id,
       type: 'stickyNote',
-      owner: 'me',
-      data: { label: label, color: colIDS[colorID], title: title },
+      data: { label: label, color: colIDS[colorID], title: title, owner: userid },
       position: safePos
     }
+    console.log(newNode)
 
     try {
       const res = await fetch("http://localhost:8000/nodes", {
@@ -157,6 +350,7 @@ function App() {
 
 
   const onNodeDragStop = async (_, node) => {
+
     const N = getIntersectingNodes(node).length
     var safePos = node.position
     console.log(N)
@@ -165,39 +359,89 @@ function App() {
       safePos = findPosition(pos.x, pos.y)
     }
     try {
-        const res = await fetch("http://localhost:8000/UpdatePosition", {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ id: node.id, position: safePos })
-        })
+      const res = await fetch("http://localhost:8000/UpdatePosition", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id: node.id, position: safePos })
+      })
 
-        if (res.ok) {
-          const data = await res.json()
-          setNodes((nodes) => nodes.map((n) => data.id === n.id ? { ...n, position: data.position } : n))
-          setCenter(data.position.x, data.position.y, { zoom: getZoom(), duration: 500 })
-          
-        }
-      } catch (err) {
-        console.log("Error: ", err)
+      if (res.ok) {
+        const data = await res.json()
+        setNodes((nodes) => nodes.map((n) => data.id === n.id ? { ...n, position: data.position } : n))
+        setCenter(data.position.x, data.position.y, { zoom: getZoom(), duration: 500 })
+
       }
+    } catch (err) {
+      console.log("Error: ", err)
+    }
   }
 
+  var nodesWithFunctions = nodes.map((node) => {
+    return {
+      ...node,
+      draggable: node.data.owner === userid,
+      data: {
+        ...node.data,
+        editNode: editNode,
+        deleteNode: deleteNode,
+        commentNode: commentNode
+      }
+    }
+  })
+
+  const fetchUserNodes = async () => {
+    try {
+      const res = await fetch(`http://localhost:8000/userNodes`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ userid: userid })
+      })
+      
+      if (res.ok) {
+        const data = await res.json()
+        console.log("User Nodes: ", data)
+        
+        return data;
+      }
+      
+    } catch (err) {
+      console.log("Error: ", err)
+      return []
+    }
+  }
+  const [userNodes, setUserNodes] = useState([])
+  const [showProfile, setShowProfile] = useState(false)
+  const openProfile = async () => {
+    setShowProfile(true)
+    const nodes= await fetchUserNodes() || []
+    setUserNodes(nodes)
+  }
+
+
+  const activeCommentNode = nodes.find(n => n.id === commentNodeId);
+  const currentComments = activeCommentNode?.data?.comments || [];
+
   return (
-    <div>
-      <header className='border-2'>
+    <div className='overflow-y-hidden'>
+      <header className='border-2 '>
         <h1 className="text-3xl align-middle text-center">Wall Of Shame</h1>
+        <button className="absolute top-1 right-1 bg-gray-100 w-8 h-8" onClick={() => openProfile()}>
+          <img src="/person.svg" alt="USER" className='w-full h-full' />
+        </button>
       </header>
       <div className='h-screen'>
-        <ReactFlow nodes={nodes} nodeTypes={nodeTypes} onNodesChange={onNodesChange} snapToGrid={true} snapGrid={[10, 10]} onNodesChange={onNodesChange} onNodeDragStop={onNodeDragStop} >
+        <ReactFlow nodes={nodesWithFunctions} nodeTypes={nodeTypes} onNodesChange={onNodesChange} snapToGrid={true} snapGrid={[10, 10]} onNodesChange={onNodesChange} onNodeDragStop={onNodeDragStop}>
           <Background />
           <Controls />
         </ReactFlow>
       </div>
       <div className='fixed bottom-10 right-10 z-50 w-64 p-4 bg-white/60 h-auto rounded border border-black'>
         <input className='w-full font-mono font-bold text-3xl' placeholder='Enter Title...' value={title} onChange={(e) => { setTitle(e.target.value) }}></input>
-        <input className='w-full font-mono text-xl' placeholder='Enter your note...' value={label} onChange={(e) => { setLabel(e.target.value) }}></input>
+        <textarea className='w-full font-mono text-xl' placeholder='Enter your note...' value={label} onChange={(e) => { setLabel(e.target.value) }}></textarea>
         <div className='flex flex-row gap-2 my-4 w-10/12  items-center justify-center mx-auto'>
           <button className={`w-12 h-12 rounded  bg-red-500 hover:bg-red-700 ${colorID == 1 ? 'ring-4 ring-black ring-offset-2' : 'ring-0'}`} onClick={() => { setColorId(1) }}></button>
           <button className={`w-12 h-12 rounded  bg-blue-500 hover:bg-blue-700 ${colorID == 2 ? 'ring-4 ring-black ring-offset-2' : 'ring-0'}`} onClick={() => { setColorId(2) }}></button>
@@ -206,6 +450,82 @@ function App() {
         </div>
         <button className='w-full rounded py-2 top-10 bg-blue-500 hover:bg-blue-700' onClick={onAddNode}>hELLO</button>
       </div>
+
+      {editNodeId && (
+        <div className='fixed top-15 right-10 z-50 w-64 p-4 bg-white/60 h-auto rounded border border-black'>
+          <h2 className='text-xl font-bold mb-2'>Edit Note</h2>
+          <input className='w-full font-mono font-bold text-3xl' placeholder='Enter Title...' value={editTitle} onChange={(e) => { setEditTitle(e.target.value) }}></input>
+          <textarea className='w-full font-mono text-xl' placeholder='Enter your note...' value={editLabel} onChange={(e) => { setEditLabel(e.target.value) }}></textarea>
+          <div className='flex flex-row gap-2 my-4 w-10/12  items-center justify-center mx-auto'>
+            <button className={`w-12 h-12 rounded  bg-red-500 hover:bg-red-700 ${editColorId == 1 ? 'ring-4 ring-black ring-offset-2' : 'ring-0'}`} onClick={() => { setEditColorId(1) }}></button>
+            <button className={`w-12 h-12 rounded  bg-blue-500 hover:bg-blue-700 ${editColorId == 2 ? 'ring-4 ring-black ring-offset-2' : 'ring-0'}`} onClick={() => { setEditColorId(2) }}></button>
+            <button className={`w-12 h-12 rounded  bg-green-500 hover:bg-redgreen700 ${editColorId == 3 ? 'ring-4 ring-black ring-offset-2' : 'ring-0'}`} onClick={() => { setEditColorId(3) }}></button>
+            <button className={`w-12 h-12 rounded  bg-yellow-500 hover:bg-red-yellow ${editColorId == 4 ? 'ring-4 ring-black ring-offset-2' : 'ring-0'}`} onClick={() => { setEditColorId(4) }}></button>
+          </div>
+          <button className='w-full rounded py-2 top-10 bg-blue-500 hover:bg-blue-700' onClick={saveEditNode}>Save</button>
+        </div>
+
+      )}
+      {commentNodeId && (
+        <div className='flex flex-col fixed bottom-10 left-10 z-50 min-w-64 max-w-96 p-4 bg-white/60 h-auto rounded border border-black'>
+          <div className="flex justify-between items-center">
+            <h2 className="font-bold text-lg font-mono">Comments | {activeCommentNode.data.title}</h2>
+            <button className="text-gray-500 hover:text-black font-bold p-1" onClick={() => setCommentNodeId(null)}>
+              X
+            </button>
+          </div>
+
+          <div className='flex-1 overflow-y-auto max-h-[60vh] flex flex-col gap-2 my-4 w-full  items-start justify-start mx-auto'>
+            {currentComments.length === 0 ? (
+              <p className='text-gray-500 italic'>No comments yet. Be the first to comment!</p>
+            ) : (
+              currentComments.map((c, index) => (
+                <div className='group width-full flex flex-row justify-between items-start'>
+                  <div key={index} className={`p-3 rounded-lg text-sm ${c.commenter === userid ? 'bg-blue-100' : 'bg-white border'}`}>
+                    <p className='font-mono text-xs font-bold block '>{c.commenter === userid ? "You" : c.commenter}</p>
+                    <p className='font-mono text-sm wrap-break-words'>{c.comment}</p>
+                  </div>
+                  <button className={`w-4 h-4 ${c.commenter === userid ? 'block' : 'hidden'} opacity-0 group-hover:opacity-100`} onClick={() => { deleteComment(c.commentid) }}>
+                    <img src="/trash.svg" alt="Delete" className='w-4 h-4' />
+                  </button>
+                </div>
+              ))
+            )}
+          </div>
+
+          <div className="bg-white border-t border-gray-200 flex flex-col gap-2">
+            <textarea className="w-full border rounded text-sm resize-none" rows="3" placeholder="Comment..." value={comment} onChange={(e) => setComment(e.target.value)}            ></textarea>
+            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 rounded transition-colors" onClick={saveComment}>
+              Post Comment
+            </button>
+          </div>
+
+
+
+          {/* <textarea className='w-full font-mono text-xl' placeholder='Enter your comment...' value={comment} onChange={(e) => { setComment(e.target.value) }}></textarea>
+          <button className='w-full rounded py-2 top-10 bg-blue-500 hover:bg-blue-700' onClick={saveComment}>Save</button> */}
+        </div>
+      )}
+      {showProfile && (
+        <div className='overflow-hidden absolute top-0 left-0 h-screen w-screen items-center justify-center bg-black/50'>
+          <div className='flex flex-col gap-4 p-4 bg-white rounded border border-black'>
+            <h2 className='text-2xl font-bold'>User Profile</h2>
+            <p><span className='font-bold'>UserID:</span> {userid}</p>
+            <p className='font-bold'>Your Codes</p>
+            <div className='border-t border-gray-300'>
+              {userNodes.map((node, index) => (
+                <div key={index} className='p-2 border rounded my-1'>
+                  <h3 className='font-bold'>{node.data.title}</h3>
+                  <p>{node.data.label}</p>
+                </div>
+              ))}
+
+            </div>
+            <button className='w-full rounded py-2 top-10 bg-blue-500 hover:bg-blue-700 text-white' onClick={() => setShowProfile(false)}>Close</button>
+          </div>
+        </div>
+
+      )}
     </div>
   )
 }
@@ -216,9 +536,10 @@ export default App
 
 {/*
 Things left to add:
-0. Connect backend with nodes data
 1. Ownership of note -> Ability to edit and drag only your nodes
+1.1 Max number of live nodes
 2. Commenting -> Delete too for owner of comment
+
 3. Profile History
 
   */}
